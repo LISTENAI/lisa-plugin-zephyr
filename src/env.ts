c4e3e02486b8c1d9a7260734d90d3b0b0cb8559c
xychen <xychen@listenai.com>
2021-10-26 16:37:41 +0800

    Fix env.ts

--- src/env.ts
+++ src/env.ts
@@ -1,20 +1,14 @@
 import { join } from 'path';
 
-const PLUGIN_HOME = join(__dirname, '..');
+export const PLUGIN_HOME = join(__dirname, '..');
 
-const VENV_HOME = join(PLUGIN_HOME, 'venv');
-const VENV_BIN = join(VENV_HOME, 'bin');
+export const VENV_HOME = join(PLUGIN_HOME, 'venv');
+export const VENV_BIN = join(VENV_HOME, 'bin');
 
-const env = () => {
+export function env() {
   return {
+    PLUGIN_HOME,
     VENV_HOME,
     VENV_BIN
   }
-}
-
-export {
-  PLUGIN_HOME,
-  VENV_HOME,
-  VENV_BIN,
-  env
 }
