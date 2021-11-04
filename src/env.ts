ea46e542875d0ad3a73d6556e88a517dce97ddda
xychen <xychen@listenai.com>
2021-11-04 21:52:44 +0800

    沙盒化 gperf

--- src/env.ts
+++ src/env.ts
@@ -13,6 +13,7 @@ const PIP_INDEX_URL = 'https://pypi.tuna.tsinghua.edu.cn/simple';
 const BUILTIN_BINARIES = [
   '@binary/cmake',
   '@binary/dtc',
+  '@binary/gperf',
   '@binary/ninja',
   '@binary/python3',
   '@binary/xz',
