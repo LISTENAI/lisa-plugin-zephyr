e89446c171154631ea3a23a6fed1262672cdfc7b
xychen <xychen@listenai.com>
2021-11-29 18:53:57 +0800

    use-sdk: 新增 --from-sdk 直接从远端初始化仓库

--- src/tasks/environment.ts
+++ src/tasks/environment.ts
@@ -77,11 +77,13 @@ export default ({ application, cmd }: LisaType) => {
       const { args, printHelp } = parseArgs(application.argv, {
         'clear': { help: '清除设置' },
         'install': { help: '安装 SDK 中的组件' },
+        'from-git': { arg: 'url#ref', help: '从指定仓库及分支初始化 SDK' },
         'task-help': { short: 'h', help: '打印帮助' },
       });
       if (args['task-help']) {
         return printHelp([
           'use-sdk [path] [--install]',
+          'use-sdk <path> --from-git https://github.com/zephyrproject-rtos/zephyr.git#main',
           'use-sdk --clear',
         ]);
       }
@@ -92,13 +94,33 @@ export default ({ application, cmd }: LisaType) => {
       } else {
         const path = argv._[1];
         const current = await get('sdk');
-        let target: string | undefined;
-        if (path && path != current) {
-          target = path;
-        } else if (args['install']) {
-          target = path || current;
+        const target = path || current;
+
+        let install = args['install'] || (path && path != current);
+
+        const fromGit = args['from-git'];
+        if (fromGit && fromGit.match(/(.+?)(?:#(.+))?$/)) {
+          const { $1: url, $2: rev } = RegExp;
+          if (!path) {
+            throw new Error('未指定 SDK 路径');
+          }
+          const workspacePath = resolve(path);
+
+          const env = await getEnv();
+          delete env.ZEPHYR_BASE;
+
+          const initArgs = ['init'];
+          initArgs.push('--manifest-url', url);
+          if (rev) initArgs.push('--manifest-rev', rev);
+          initArgs.push(workspacePath);
+          await exec('python', ['-m', 'west', ...initArgs], { env });
+
+          await exec('python', ['-m', 'west', 'update'], { env, cwd: workspacePath });
+
+          install = true;
         }
-        if (target) {
+
+        if (target && install) {
           let zephyrPath = resolve(target);
           if (!(await zephyrVersion(zephyrPath))) {
             zephyrPath = join(zephyrPath, 'zephyr');
