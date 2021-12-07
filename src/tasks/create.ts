38ff56f40e9de9ae37f5acaca2020f555cc4790d
zhaozhuobin <zbzhao2@iflytek.com>
2021-12-07 21:00:50 +0800

    [Feature]create from sample

--- src/tasks/create.ts
+++ src/tasks/create.ts
@@ -1,16 +1,19 @@
 import { LisaType, job } from '../utils/lisa_ex';
 import { get } from '../env/config';
-import { join } from 'path';
-import { pathExists, createReadStream } from 'fs-extra';
+import { join, parse, resolve, sep } from 'path';
+import { pathExists, createReadStream, copy, mkdirs } from 'fs-extra';
 import { createInterface } from 'readline';
 import { once } from 'events';
 import * as glob from 'glob';
+import { ISampleList, path2json } from '../utils/fs';
+import { promptDir } from '../utils/ux';
 
 export default ({ application, cmd }: LisaType) => {
   job('create', {
     title: '创建sample',
     async task(ctx, task) {
       const sdk = await get('sdk') || '';
+      // Feature: sample.list的位置获取方式
       const sampleListFile = join(sdk || '', './samples/boards/csk6001/sample.list');      
       application.debug(sampleListFile);
       if (!(await pathExists(sampleListFile))) {
@@ -25,26 +28,36 @@ export default ({ application, cmd }: LisaType) => {
       rl.on('line', async (line) => {
         line = line.trim();
         if (line && !line.startsWith('#')) {
-          sampleList.push(join(sdk, line));
-          // application.debug(await pathExists(join(sdk, line)));
+          line = !line.endsWith('*') ? join(sdk, line, './**/CMakeLists.txt') : join(sdk, `${line}*`, './CMakeLists.txt');
+          sampleList.push(resolve(line));
         }
       });
 
       await once(rl, 'close');
 
+      let sampleListJson: ISampleList = {};
+
       for (const samplePath of sampleList) {
-        application.debug(samplePath);
-        // const sampleCMakeFile = join(samplePath, './CMakeLists.txt');
         const files = glob.sync(samplePath, {})
-        application.debug(files);
+        for (const file of files) {
+          const dirParse = resolve(parse(file).dir).replace(sdk, '').split(sep);
+          sampleListJson = await path2json(dirParse, sampleListJson);
+        }
       }
 
-
-      // for await (const line of )
+      // 根据sampleListJson ux.select 嵌套
+      application.debug(sampleListJson);
+      const selected = await promptDir([], sampleListJson, task)
+      const selectedSample = join(sdk, ...selected);
       
-      application.debug('end');
+      const targetDir = join(process.cwd(), await task.prompt({
+        type: 'Input',
+        message: '创建文件夹名',
+        initial: selected.pop() as string
+      }));
 
-    
+      await mkdirs(targetDir);
+      await copy(selectedSample, targetDir);
     }
   });
 }
--- src/utils/ux.ts
+++ src/utils/ux.ts
@@ -1,9 +1,32 @@
-import LISA from '@listenai/lisa_core'
-import { ParsedArgs } from 'minimist'
-import { resolve } from 'path'
+import LISA, { TaskObject } from '@listenai/lisa_core';
+import { ParsedArgs } from 'minimist';
+import { resolve } from 'path';
+import {ISampleList} from './fs';
 
 export function workspace(): string | undefined {
   const { application } = LISA;
   const argv = application.argv as ParsedArgs;
   return argv._[1] ? resolve(argv._[1]) : undefined;
+}
+
+type TaskArguments = Parameters<TaskObject['task']>;
+
+export async function promptDir(selected: Array<string>, dirJson: ISampleList, task: TaskArguments[1]): Promise<Array<string>> {
+  const choices = Object.keys(dirJson).map(dir => {
+    return typeof dirJson[dir] !== 'string' ? `${dir}/` : dir;
+  });
+  const value = await task.prompt<string>([
+    {
+      type: 'Select',
+      name: 'value',
+      message: 'Please select dir',
+      choices: choices,
+      result: (value) => value.replace('/', '')
+    }
+  ])
+  selected.push(value);
+  if (typeof dirJson[value] === 'string') {
+    return selected
+  }
+  return await promptDir(selected, dirJson[value] as ISampleList, task);
 }
