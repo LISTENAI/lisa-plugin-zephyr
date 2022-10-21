d964cb368557776c87bea6c1c9a115a1ead21423
zhaozhuobin <zbzhao2@iflytek.com>
2022-10-21 10:35:01 +0800

    优化lisa zep sdk更换版本时tag同步问题，增加 lisa zep install 走在线参数

--- src/tasks/installation.ts
+++ src/tasks/installation.ts
@@ -1,6 +1,8 @@
 import { LisaType, job } from "../utils/lisa_ex";
 import { join, resolve } from "path";
-import { mkdirs, remove, pathExists } from "fs-extra";
+import { mkdirs, remove, pathExists, rename } from "fs-extra";
+import { ParsedArgs } from "minimist";
+import parseArgs from "../utils/parseArgs";
 
 import extendExec from "../utils/extendExec";
 import { getEnv, invalidateEnv } from "../env";
@@ -17,17 +19,28 @@ export default ({ cmd, application }: LisaType) => {
       const exec = extendExec(cmd, { task });
       await mkdirs(PLUGIN_HOME);
       await invalidateEnv();
-      const requirementsPath = join(process.env.LISA_HOME || "", "lisa-zephyr", "whl");
+      const whlPath = join(process.env.LISA_HOME || "", "lisa-zephyr", "whl");
+      const requirementsPath = join(whlPath, "local_requirements.txt");
       const isExists = await pathExists(requirementsPath);
+      const argv = application.argv as ParsedArgs;
+      const { args, printHelp } = parseArgs(application.argv, {
+        // "task-help": { short: "h", help: "打印帮助" },
+        "online": { arg: "online", help: "python包走在线安装" },
+      });
       application.debug(`PLUGIN_HOME:${PLUGIN_HOME},venv.homeDir:${venv.homeDir},requirementsPath:${requirementsPath},isExists:${isExists}`);
-      // console.log(`PLUGIN_HOME:${PLUGIN_HOME},venv.homeDir:${venv.homeDir},requirementsPath:${requirementsPath},isExists:${isExists}`)
+      
+
+      const online = args.online
+      
       await exec(join(python.binaryDir, "python"), [
         "-m",
         "venv",
         venv.homeDir,
       ]);
+
+      application.debug(`存在:${isExists}，在线:${online}`);
       //装requirement.txt whl的离线包
-      isExists ? await exec("python", ["-m", "pip", "install", "-r", join(requirementsPath, "local_requirements.txt"), "-f", "./dependencies", "--no-index"], {
+      (isExists && !online) ? await exec("python", ["-m", "pip", "install", "-r", requirementsPath, "-f", "./dependencies", "--no-index"], {
         env: await getEnv(),
         cwd: requirementsPath
       }) :
@@ -41,6 +54,9 @@ export default ({ cmd, application }: LisaType) => {
       );
 
       await invalidateEnv();
+      if (isExists) {
+        await rename(requirementsPath, join(whlPath, "local_requirements_back.txt"))
+      }
       testLog(task, "安装成功");
     },
   });
